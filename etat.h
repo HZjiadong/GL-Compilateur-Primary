5ee5fd132d76b2dec4d4d7cf838a939f73ebc237
HZjiadong <guojiadonghz@gmail.com>
2021-02-22 18:22:47 +0800

    Create etat.h

---
+++ etat.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "symbole.h"
+using namesapce std;
+
+
+class Etat1;
+class Etat2;
+class Etat3;
+class Etat4;
+class Etat5;
+class Etat6;
+class Etat7;
+class Etat8;
+class Etat9;
+
+class Etat {
+public:
+	Etat(string name);
+	virtual ~Etat();
+	void print() const;
+	virtual bool transition(Automate & automate, Symbole * s) = 0;
+
+protected:
+	string name;
+};
